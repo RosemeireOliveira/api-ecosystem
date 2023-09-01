d56eea792be0b457b354f1f268887e782c3a4afc
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2023-08-31 21:44:55 -0300

    conclusão do index.tsx, css e imports

--- src/pages/BuscarPublicacoes/index.tsx
+++ src/pages/BuscarPublicacoes/index.tsx
@@ -1,11 +1,128 @@
 import './style.css'
+import image12 from "../../assets/img/image 12.png"
+import image8 from "../../assets/img/image 8.png"
+import image9 from "../../assets/img/image 9.png"
+import image15 from "../../assets/img/image 15.png"
 
-function BuscarPublicacoes (){
+function BuscarPublicacoes() {
 
 
     return (
         <>
-        
+            <main>
+                <h1>página buscar por publicações ecosystem &amp; recycle</h1>
+                <section>
+                    <div className="conteudo_doacoes wrapper">
+                        <aside>
+                            <ul>
+                                <li>
+                                    <a href="../Tela_Coletas_Agendadas/index.html">coletas agendadas</a>
+                                </li>
+                                <li>
+                                    <a href="../Tela_Buscar_Publicacoes/index.html">
+                                        Buscar por publicações
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="../Tela_Editar_Perfil_Coletor/index.html">editar perfil</a>
+                                </li>
+                            </ul>
+                        </aside>
+                        <div className="menu_Direito">
+                            <div className="title">
+                                <h2>Buscar por Publicações</h2>
+                                <p>Veja todas as publicações ativas mais próximas de você!</p>
+                            </div>
+                            <div>
+                                <div id="um" className="card">
+                                    <div>
+                                        <img src={image12} alt="" />
+                                    </div>
+                                    <div className="conteudoCard">
+                                        <div className="cardInformacao">
+                                            <h2>celulares diversos</h2>
+                                            <p>Data de publicação 30/03/2023</p>
+                                            <p>quandidade de iten(s): 4</p>
+                                            <div>
+                                                <p>Descrição: </p>
+                                                <div>
+                                                    <p>1x Celular Samsung Modelo J5 Prime</p>
+                                                    <p>1x Iphone 4 Plus...</p>
+                                                </div>
+                                            </div>
+                                            <p>sbc - sp</p>
+                                        </div>
+                                        <a href="../Tela_Agendar_Coleta/index.html">Agendar</a>
+                                    </div>
+                                </div>
+                                <div id="dois" className="card">
+                                    <div>
+                                        <img src={image8} alt="" />
+                                    </div>
+                                    <div className="conteudoCard">
+                                        <div className="cardInformacao">
+                                            <h2>Celulares antigos de diversas marcas e modelos</h2>
+                                            <p>Data de publicação 29/03/2023</p>
+                                            <p>quandidade de iten(s): 39</p>
+                                            <div>
+                                                <p>Descrição: </p>
+                                                <div>
+                                                    <p>1x Celular Samsung Modelo J5 Prime</p>
+                                                    <p>3x LG 320+...</p>
+                                                </div>
+                                            </div>
+                                            <p>sbc - sp</p>
+                                        </div>
+                                        <a href="../Tela_Agendar_Coleta/index.html">Agendar</a>
+                                    </div>
+                                </div>
+                                <div id="tres" className="card">
+                                    <div>
+                                        <img src={image9} alt="" />
+                                    </div>
+                                    <div className="conteudoCard">
+                                        <div className="cardInformacao">
+                                            <h2>placas e circuitos eletrônicos</h2>
+                                            <p>Data de publicação 30/03/2023</p>
+                                            <p>quandidade de iten(s): 25</p>
+                                            <div>
+                                                <p>Descrição: </p>
+                                                <div>
+                                                    <p>Diversos...</p>
+                                                </div>
+                                            </div>
+                                            <p>diadema - sp</p>
+                                        </div>
+                                        <a href="../Tela_Agendar_Coleta/index.html">Agendar</a>
+                                    </div>
+                                </div>
+                                <div id="quatro" className="card">
+                                    <div>
+                                        <img src={image15} alt="" />
+                                    </div>
+                                    <div className="conteudoCard">
+                                        <div className="cardInformacao">
+                                            <h2>CPU's e Placas mãe</h2>
+                                            <p>Data de publicação 28/03/2023</p>
+                                            <p>quandidade de iten(s): 15</p>
+                                            <div>
+                                                <p>Descrição: </p>
+                                                <div>
+                                                    <p>10x CPUs diversos modelos</p>
+                                                    <p>5x Placas diversas...</p>
+                                                </div>
+                                            </div>
+                                            <p>diadema - sp</p>
+                                        </div>
+                                        <a href="../Tela_Agendar_Coleta/index.html">Agendar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
         </>
     )
 }
