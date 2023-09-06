51254a34e8b54e5b800a246c4ff3c34aba7ed231
Alcides Paz <alcidesnp@hotmail.com>
2023-09-06 18:28:50 -0300

    Adicinado novos componentes

--- src/pages/AgendarColeta/index.tsx
+++ src/pages/AgendarColeta/index.tsx
@@ -1,23 +1,26 @@
 import './style.css';
-import img_card_001 from '../../assets/img/img_card_001.png';
+
 import img_card_002 from '../../assets/img/img_card_002.png'
 import ico_edit from '../../assets/img/ico_edit.svg';
 import ico_eye from '../../assets/img/ico_eye.svg'
 import ico_delete from '../../assets/img/ico_delete.svg'
+import Aside from '../../components/Aside';
+import Card from '../../components/Card';
 import { Link, useNavigate } from 'react-router-dom';
+
 
 
 function AgendarColeta() {
 
-        let navigate = useNavigate()
+    let navigate = useNavigate()
 
-    function click(){
+    function click() {
         navigate('/home')
     }
 
-    function msgDeletarColeta(){
+    function msgDeletarColeta() {
         alert('A doação foi cancelada! O doador irá ser notificado');
-      };
+    };
 
     return (
         <>
@@ -25,21 +28,9 @@ function AgendarColeta() {
                 <h1>página coletas agendadas ecosystem &amp; recycle</h1>
                 <section>
                     <div className="conteudo_doacoes wrapper">
-                        <aside>
-                            <ul>
-                                <li>
-                                    <a href="../Tela_Coletas_Agendadas/index.html">coletas agendadas</a>
-                                </li>
-                                <li>
-                                    <a href="../Tela_Buscar_Publicacoes/index.html">
-                                        Buscar por publicações
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="../Tela_Editar_Perfil_Coletor/index.html">editar perfil</a>
-                                </li>
-                            </ul>
-                        </aside>
+                        {/* Abaixo está sendo chamado o  componente aside */}
+                        <Aside />
+
                         <div className="menu_Direito">
                             <div className="title">
                                 <h2>coletas agendadas</h2>
@@ -55,52 +46,31 @@ function AgendarColeta() {
                                 </div>
                                 <div className="historic_cards">
                                     <div className="Conteudo_Cards">
-                                        <div className="cards">
-                                            <h4>Celulares antigos</h4>
-                                            <img
-                                                src= {img_card_001}
-                                                alt="Imagem com sucata de aparelhos celulares gsm"
-                                            />
-                                            <div className="WrapperCard">
-                                                <p>Data de publicação: 30/03/2023</p>
-                                                <p>Quantidade de iten(s): 39</p>
-                                                <p>Responsável: Luís</p>
-                                                <div className="Descriocaocards">
-                                                    <p>Descrição: </p>
-                                                    <div>
-                                                        <p>5x Celular Samsung A5</p>
-                                                        <p>2x Iphone 2 ...</p>
-                                                    </div>
-                                                </div>
-                                                <p>
-                                                    Localização: <strong>SBC-SP</strong>
-                                                </p>
-                                                <div className="card_Status">
-                                                    <div className="circle_Status circle_Yellow"></div>
-                                                    <p>
-                                                        <span>Status: Aguardando confirmação de horário </span>{" "}
-                                                    </p>
-                                                </div>
-                                                <div className="iconesCards">
-                                                    <a href="../Tela_Agendar_Coleta/index.html">
-                                                        <img src={ico_edit}
-                                                         alt="Icone de Editar" />
-                                                    </a>
-                                                    <a href="../Tela_Agendar_Coleta/index.html">
-                                                        <img
-                                                            src= {ico_eye}
-                                                            alt="Icone de visualizar"
-                                                        />
-                                                    </a>
-                                                    {/* <a href="#" onClick={msgDeletarColeta()}>
-                                                        <img
-                                                            src= {ico_delete}
-                                                            alt="Icone de Deletar"
-                                                        />
-                                                    </a> */}
-                                                </div>
-                                            </div>
-                                        </div>
+
+                                        {/* Importação do Card */}
+                                        <Card 
+                                        tituloCard = 'Celulares antigos'
+                                        conteudoCardData = '30/03/2023'
+                                        conteudoCardQuantidade = '3'
+                                        conteudoCardOwner = 'Luís'
+                                        quantidadeProduto1 = '5'
+                                        descricao1 = 'Celular Samsung A5'
+                                        quantidadeProduto2 = '2'
+                                        descricao2 = 'Iphone 2'
+                                        localizacao = 'SBC-SP'
+                                        />
+                                        <Card 
+                                             tituloCard = 'Celulares novos'
+                                             conteudoCardData = '30/03/2023'
+                                             conteudoCardQuantidade = '3'
+                                             conteudoCardOwner = 'Luís'
+                                             quantidadeProduto1 = '5'
+                                             descricao1 = 'Celular Samsung A5'
+                                             quantidadeProduto2 = '2'
+                                             descricao2 = 'Iphone 2'
+                                             localizacao = 'SBC-SP'/>
+                                        <Card />
+
                                         <div className="cards">
                                             <h4>Celulares diversos</h4>
                                             <img
@@ -130,11 +100,11 @@ function AgendarColeta() {
                                                 </div>
                                                 <div className="iconesCards">
                                                     <a href="../Tela_Agendar_Coleta/index.html">
-                                                        <img src= {ico_edit} alt="Icone de Editar" />
+                                                        <img src={ico_edit} alt="Icone de Editar" />
                                                     </a>
                                                     <a href="../Tela_Agendar_Coleta/index.html">
                                                         <img
-                                                            src= {ico_eye}
+                                                            src={ico_eye}
                                                             alt="Icone de visualizar"
                                                         />
                                                     </a>
@@ -148,14 +118,14 @@ function AgendarColeta() {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="btnVoltar">
+                                    {/* <div className="btnVoltar">
                                         <Link to ={ click() }>voltar</Link>
-                                    </div>
+                                    </div> */}
                                 </div>
                             </div>
                         </div>
                     </div>
-                </section>  
+                </section>
             </main>
 
 
