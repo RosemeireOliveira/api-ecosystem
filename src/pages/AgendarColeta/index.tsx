473db4ea76a2c0175a70c7cb6f57df5d1097dfec
alcides Paz <alcidesnp@hotmail.com>
2023-09-06 21:59:22 -0300

    Atualização da Página

--- src/pages/AgendarColeta/index.tsx
+++ src/pages/AgendarColeta/index.tsx
@@ -11,125 +11,90 @@ import { Link, useNavigate } from 'react-router-dom';
 
 
 function AgendarColeta() {
-
-    let navigate = useNavigate()
-
-    function click() {
-        navigate('/home')
-    }
-
-    function msgDeletarColeta() {
-        alert('A doação foi cancelada! O doador irá ser notificado');
-    };
-
     return (
-        <>
-            <main>
-                <h1>página coletas agendadas ecosystem &amp; recycle</h1>
-                <section>
-                    <div className="conteudo_doacoes wrapper">
-                        {/* Abaixo está sendo chamado o  componente aside */}
-                        <Aside />
+        <main>
+            <h1>página agendar coleta ecosystem &amp; recycle</h1>
+            <section>
+                <div className="conteudo_doacoes wrapper">
+                    {/* <aside>
+                        <ul>
+                            <li>
+                                <a href="../Tela_Coletas_Agendadas/index.html">coletas agendadas</a>
+                            </li>
+                            <li>
+                                <a href="../Tela_Buscar_Publicacoes/index.html">
+                                    Buscar por publicações
+                                </a>
+                            </li>
+                            <li>
+                                <a href="../Tela_Editar_Perfil_Coletor/index.html">editar perfil</a>
+                            </li>
+                        </ul>
+                    </aside> */}
 
-                        <div className="menu_Direito">
-                            <div className="title">
-                                <h2>coletas agendadas</h2>
-                                <p>Visualize o histórico e agendamentos ativos aqui</p>
-                            </div>
-                            <div>
-                                <div className="links_pags">
-                                    <a href="../Tela_Coletas_Agendadas/index.html">coletas ativas</a>
-                                    <span>|</span>
-                                    <a href="../Tela_Coletas_Finalizadas/index.html">
-                                        coletas finalizadas
-                                    </a>
+                < Aside />
+                    <div className="conteudoPublicacoes">
+                        <div className="headerPublicacoes">
+                            <h2>Agendar Coleta</h2>
+                            <p>Agende aqui a sua retirada do material</p>
+                        </div>
+                        <div className="containerCard">
+                            <div className="card">
+                                <div className="cardEsquerda">
+                                    <h2>Celulares antigos de diversas marcas e modelos</h2>
+                                    <img src="../../img/image 12.png" alt="" />
+                                    <p>Agendar Retirada</p>
                                 </div>
-                                <div className="historic_cards">
-                                    <div className="Conteudo_Cards">
-
-                                        {/* Importação do Card */}
-                                        <Card 
-                                        tituloCard = 'Celulares antigos'
-                                        conteudoCardData = '30/03/2023'
-                                        conteudoCardQuantidade = '3'
-                                        conteudoCardOwner = 'Luís'
-                                        quantidadeProduto1 = '5'
-                                        descricao1 = 'Celular Samsung A5'
-                                        quantidadeProduto2 = '2'
-                                        descricao2 = 'Iphone 2'
-                                        localizacao = 'SBC-SP'
-                                        />
-                                        <Card 
-                                             tituloCard = 'Celulares novos'
-                                             conteudoCardData = '30/03/2023'
-                                             conteudoCardQuantidade = '3'
-                                             conteudoCardOwner = 'Luís'
-                                             quantidadeProduto1 = '5'
-                                             descricao1 = 'Celular Samsung A5'
-                                             quantidadeProduto2 = '2'
-                                             descricao2 = 'Iphone 2'
-                                             localizacao = 'SBC-SP'/>
-                                        <Card />
-
-                                        <div className="cards">
-                                            <h4>Celulares diversos</h4>
-                                            <img
-                                                src={img_card_002}
-                                                alt="Imagem com sucata de aparelhos celulares gsm"
-                                            />
-                                            <div className="WrapperCard">
-                                                <p>Data de publicação: 25/03/2023</p>
-                                                <p>Quantidade de iten(s): 4</p>
-                                                <p>Responsável: Geraldo</p>
-                                                <div className="Descriocaocards">
-                                                    <p>Descrição: </p>
-                                                    <div>
-                                                        <p>1x Celular Samsung modelo J5 prime</p>
-                                                        <p>1x Iphone 4 Plus... ...</p>
-                                                    </div>
-                                                </div>
-                                                <p>
-                                                    Localização: <strong>Diadema-SP</strong>
-                                                </p>
-                                                <div className="card_Status">
-                                                    <div className="circle_Status circle_Orange"></div>
-                                                    <p>
-                                                        <span>Status:</span> Publicação agendada! 15/05/23 -
-                                                        10:00hrs
-                                                    </p>
-                                                </div>
-                                                <div className="iconesCards">
-                                                    <a href="../Tela_Agendar_Coleta/index.html">
-                                                        <img src={ico_edit} alt="Icone de Editar" />
-                                                    </a>
-                                                    <a href="../Tela_Agendar_Coleta/index.html">
-                                                        <img
-                                                            src={ico_eye}
-                                                            alt="Icone de visualizar"
-                                                        />
-                                                    </a>
-                                                    {/* <a href="#" onClick= {msgDeletarColeta()}>
-                                                        <img
-                                                            src= { ico_delete }
-                                                            alt="Icone de Deletar"
-                                                        />
-                                                    </a> */}
-                                                </div>
+                                <div className="cardDireita">
+                                    <div className="cardDireitaConteudo">
+                                        <h2>Dados da Retirada</h2>
+                                        <p>Data de publicação 30/03/2023</p>
+                                        <p>quandidade de iten(s): 39</p>
+                                        <div>
+                                            <p>Descrição: </p>
+                                            <div>
+                                                <p>5x Celular Samsung A5</p>
+                                                <p>2x Iphone 2</p>
+                                                <p>7x Celular LG 320</p>
+                                                <p>3x Tablet Samsung Galaxy Tab 2</p>
+                                                <p>...</p>
                                             </div>
                                         </div>
+                                        <p>Condição: Para desmontagem/Retirada de peças/p&gt;</p>
+                                        <p>
+                                            Localização: Av. do Brasil, N° 1000 - apt25/Bloco D - SBC - SP
+                                            CEP: 12345-678
+                                        </p>
+                                        <p>
+                                            Disponibilidade de horário para agendamento: Seg - Sex / 09:00
+                                            às 14:00
+                                        </p>
                                     </div>
-                                    {/* <div className="btnVoltar">
-                                        <Link to ={ click() }>voltar</Link>
-                                    </div> */}
                                 </div>
                             </div>
+                            <div className="cardBottom">
+                                <div className="cardBottomPt1">
+                                    <div>
+                                        <label>Data:</label>
+                                        <textarea defaultValue={""} />
+                                    </div>
+                                    <div>
+                                        <label>Horário</label>
+                                        <textarea defaultValue={""} />
+                                    </div>
+                                </div>
+                                <div className="cardBottomPt2">
+                                    <label>Observação</label>
+                                    <textarea defaultValue={""} />
+                                </div>
+                            </div>
+                            <a href="../Tela_Coletas_Agendadas/index.html">Agendar</a>
                         </div>
                     </div>
-                </section>
-            </main>
+                </div>
+            </section>
+        </main>
 
-
-        </>
     )
 }
 
